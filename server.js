a4b1ee6687739a684be4338e99d25d31fa1678e0
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-03 17:25:02 -0400

    added ./models/index.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set("view engine", "handlebars");
 
 // Requiring our routes
 require("./routes/html.js")(app);
-require("./routes/api.js")(app);
+// require("./routes/api.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
