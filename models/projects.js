fffa9d1c174e78cb3236cb15cd89633f0915c39d
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-03 18:03:49 -0400

    created model/projects.js, database populate cols success.

---
+++ models/projects.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize, DataTypes) {
+    const Project = sequelize.define("Project", {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        url_code: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        live_preview: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isUrl: true
+            }
+        },
+        project_updated_at: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+    });
+
+    return Project;
+};
