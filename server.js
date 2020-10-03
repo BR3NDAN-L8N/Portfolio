edf22505933c574d80f153337596179704b4a830
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-03 16:28:20 -0400

    added server.js with boiler

---
+++ server.js
@@ -0,0 +1,30 @@
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+// Creating express app
+const app = express();
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Requiring our routes
+require("./routes/html.js")(app);
+require("./routes/api.js")(app);
+
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
