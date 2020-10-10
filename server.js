356695cde5afab10304176cab087b0b65b57ef56
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-10 15:38:20 -0400

    added projects to handlebars, as well as images. Added mongoose for database.

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-// Requiring necessary npm packages
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const path = require("path");
 
-// Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
+
 const db = require("./models");
 
-// Creating express app
+// Set Express app
 const app = express();
-app.use(express.static(path.join(__dirname,'/public/')))
 
-// Set Handlebars.
+// Set Handlebars
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Requiring our routes
-require("./routes/html.js")(app);
-// require("./routes/api.js")(app);
+app.use(logger("dev"));
 
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-portfolio", { useNewUrlParser: true });
+
+// require routes
+require("./routes/html.js")(app);
+require("./routes/api.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
