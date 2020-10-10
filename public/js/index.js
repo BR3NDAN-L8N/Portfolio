a557a3ce56c9ea8b47f6cb3e8b5860cbae6b73ad
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-10 19:06:38 -0400

    added overlay for emails

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,25 @@
 $('document').ready(function () {
-    $('body').scrollspy({
-        target: '#navbar',
-        offset: 50
-      });
+
+  //  NAVBAR CODE
+  $('body').scrollspy({
+    target: '#navbar',
+    offset: 50
+  });
+
+  $(".email-button").on("click", function () {
+    $(".email-button").addClass("hide");
+    $(".email-form").removeClass("hide");
+  });
+  $(".exit-button").on("click", function () {
+    $(".email-form").addClass("hide");
+    $(".email-button").removeClass("hide");
+  });
+  $("#submit-email-button").on("click", function () {
+    const emailData = {
+      email: $("#email"),
+      name: $("#name"),
+      message: $("")
+    }
+  })
+
 })
