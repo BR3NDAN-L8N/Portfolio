98de270c4b7fddf9faaae95a997b23a9481d88a9
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-03-20 19:54:11 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 //  COMPONENT imports
 import About from "./AppSections/About/About";
+import Navbar from "./components/Navbar/Navbar";
 
 import './App.css';
 
 function App() {
   return (
-    <main className="App">
+    <main className="App" id="scroll-top">
+          <Navbar />
           <header className="App-header">
               <h1 className="container">
                   Hey, I'm <span className="hero-name">Brendan Leighton!</span> Web Developer
