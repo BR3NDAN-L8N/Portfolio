02af0e20f7aa11191b25338047c0c23f2c490e52
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-03-20 16:42:12 -0400

    created basic header for sections of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+//  COMPONENT imports
+import SectionHeader from "./components/Section Header/SectionHeader";
+
 import './App.css';
 
 function App() {
   return (
     <main className="App">
-      <header className="App-header">Ha Ha Header</header>
+          <header className="App-header">Hero Header</header>
+          
+          <SectionHeader 
+            title="About"
+          />
     </main>
   );
 }
