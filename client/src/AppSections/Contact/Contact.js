abad01aaee9c468376d014b934c9ab4a165b59ea
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-03-23 17:31:40 -0400

    not all changes from last commit were staged

--- client/src/AppSections/Contact/Contact.js
+++ client/src/AppSections/Contact/Contact.js
@@ -12,8 +12,24 @@ export default function Contact() {
     //  FORM
     const { register, handleSubmit, error } = useForm();
 
-    const configureEmail = (data) => {
+    const postToBackendApi = (data) => {
         console.log(data);
+        const url = 'email/send-email';
+        const initObject = {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data) // body data type must match "Content-Type" header
+        };
+        fetch(url, initObject);
+        // return response.json;
     }
 
     return (
@@ -23,7 +39,7 @@ export default function Contact() {
                 title="Get In Touch!"
             />
 
-            <form className="form" onSubmit={handleSubmit(configureEmail)}>
+            <form className="form" onSubmit={handleSubmit(postToBackendApi)}>
                 <div className="form-wrapper">
                     {/* FORM LEFT */}
                     <div className="form-left">
