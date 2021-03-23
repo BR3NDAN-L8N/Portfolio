2b20cf437ed7b790c4614f94fdaf3e350a042f98
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-03-23 17:01:17 -0400

    Added files for SendGrid API from old portfolio.

---
+++ Server/SendGridApi/SendGridApi.js
@@ -0,0 +1,75 @@
+const sgMail = require('@sendgrid/mail'),
+    fs = require("fs"),
+    Handlebars = require("handlebars"),
+    path = require("path");
+require('dotenv').config();
+
+sgMail.setApiKey(process.env.SendGridAPI);
+
+sendEmail = (msg) => {
+    (async () => {
+        try {
+            await sgMail.send(msg);
+        } catch (error) {
+            console.error(error);
+
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        }
+    })();
+}
+
+getTemplate = (emailTemplate) => {
+    // Open template file
+    const chosenTemplate = fs.readFileSync(path.join(__dirname, `../views/email/email-to-${emailTemplate}.handlebars`), 'utf8');
+    // Create email generator
+    return template = Handlebars.compile(chosenTemplate);
+}
+
+toAdmin = ({ email, name, message } = emailData) => {
+
+    const data = {
+        email,
+        name,
+        message
+    }
+
+    const template = getTemplate("admin");
+    emailHTML = template(data);
+
+    const msg = {
+        to: process.env.SendGridAdminEmail,
+        from: process.env.SendGridAdminEmail,
+        subject: `${name} Might Have A Job For You!`,
+        html: emailHTML
+    };
+
+    sendEmail(msg);
+}
+
+toUser = ({ email, name, message } = emailData) => {
+
+    const data = {
+        email,
+        name,
+        message
+    }
+
+    const template = getTemplate("user");
+    emailHTML = template(data);
+
+    const msg = {
+        to: email,
+        from: process.env.SendGridAdminEmail,
+        subject: "Your Hiring Journey Is Almost Over!",
+        html: emailHTML
+    };
+
+    sendEmail(msg);
+}
+
+module.exports = {
+    toAdmin,
+    toUser
+};
---
+++ Server/SendGridApi/apiRoute.js
@@ -0,0 +1,9 @@
+// Requiring path to so we can use relative routes to our HTML files
+const sendEmail = require("../config/email-orm");
+
+module.exports = function (app) {
+    app.post("/email/send-email", (req, res) => {
+        sendEmail.toAdmin(req.body);
+        sendEmail.toUser(req.body);
+    });
+};
