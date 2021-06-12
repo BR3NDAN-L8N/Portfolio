33c14c9060a1d6d5f8363edae64ec5d0e7629ac4
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-06-12 10:17:34 -0400

    working on desktop grid layout. Removed styling from hero component and added it to the newly created Hero.css

--- client/src/AppSections/Hero/Hero.js
+++ client/src/AppSections/Hero/Hero.js
@@ -2,51 +2,20 @@ import React from 'react'
 
 import CTA from '../../components/CallToAction/CTA'
 import ContactStrip from '../../components/Contacts/ContactStrip'
-import imageOfMe from './assets/me.jpeg';
+import imageOfMe from './assets/me.jpeg'
 
-const styles = {
-    hero: {
-        height: `100vh`,
-        textAlign: `center`
-    },
-    heroFlex: {
-        display: `flex`,
-        flexDirection: `column`,
-        justifyContent: `center`,
-        gap: `2rem`
-    },
-    figure: {
-        display: `flex`,
-        justifyContent: `center`
-    },
-    image: {
-        height: `192px`,
-        width: `192px`,
-        margin: `0, auto`,
-        boxShadow: `0px 7px 10px rgba(0, 0, 0, 0.5)`,
-        borderRadius: `50%`
-    },
-    h1: {
-        fontWeight: `800`,
-        textAlign: `center`
-    },
-    heroSubtitle: {
-        fontWeight: `400`,
-        padding: 'none',
-        margin: 'none'
-    }
-}
+import './Hero.css'
 
 export default function Hero() {
     return (
-        <section style={styles.hero} className="container grid-hero">
-            <header style={styles.heroFlex}>
-                <figure style={styles.figure}>
-                    <img style={styles.image} src={imageOfMe}
+        <section className="container hero grid-hero">
+            <header>
+                <figure>
+                    <img src={imageOfMe} className="hero-image"
                         alt="Me smiling, rocking a bushy beard and a mohawk" />
                 </figure>
-                <h1 style={styles.h1}>Brendan Leighton</h1>
-                <p style={styles.heroSubtitle}>Web Engineer</p>
+                <h1>Brendan Leighton</h1>
+                <p>Web Engineer</p>
                 <ContactStrip 
                     size="1.3rem"
                 />
